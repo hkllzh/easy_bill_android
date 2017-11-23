f178546fd824f29e72989923788feb72ba047042
lizheng <li396858359@163.com>
2017-11-23 20:18:41 +0800

    init

---
+++ app/src/main/java/com/hkllzh/easybill/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hkllzh.easybill
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
