6128d8405528c8c30ff6ff8114381436bb96a567
lizheng <li396858359@163.com>
2017-11-23 20:38:35 +0800

    再次增加Anko库

--- app/src/main/java/com/hkllzh/easybill/MainActivity.kt
+++ app/src/main/java/com/hkllzh/easybill/MainActivity.kt
@@ -2,11 +2,20 @@ package com.hkllzh.easybill
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnNoActionVersion.setOnClickListener {
+            toast("进入无 Action Bar 的版本")
+        }
+
+
+
     }
 }
